ab5bb30ab6b3e100ae3c1c42f832e42cbe0add1e
shjy2008 <shjy2011@163.com>
2025-04-25 22:46:49 +1200

    test performance on MedQA

--- train/test_performance.py
+++ train/test_performance.py
@@ -36,56 +36,78 @@ class TestPerformance():
         {{choices}}
         '''
 
+        examples = [
         # Training data index 0 (GBaker/MedQA-USMLE-4-options)
-        question = "A 23-year-old pregnant woman at 22 weeks gestation presents with burning upon urination. She states it started 1 day ago and has been worsening despite drinking more water and taking cranberry extract. She otherwise feels well and is followed by a doctor for her pregnancy. Her temperature is 97.7°F (36.5°C), blood pressure is 122/77 mmHg, pulse is 80/min, respirations are 19/min, and oxygen saturation is 98% on room air. Physical exam is notable for an absence of costovertebral angle tenderness and a gravid uterus. Which of the following is the best treatment for this patient?"
-        choices = {
+        # correct: D
+        {"question": "A 23-year-old pregnant woman at 22 weeks gestation presents with burning upon urination. She states it started 1 day ago and has been worsening despite drinking more water and taking cranberry extract. She otherwise feels well and is followed by a doctor for her pregnancy. Her temperature is 97.7°F (36.5°C), blood pressure is 122/77 mmHg, pulse is 80/min, respirations are 19/min, and oxygen saturation is 98% on room air. Physical exam is notable for an absence of costovertebral angle tenderness and a gravid uterus. Which of the following is the best treatment for this patient?",
+        "choices": {
         "A": "Ampicillin",
         "B": "Ceftriaxone",
         "C": "Doxycycline",
         "D": "Nitrofurantoin"
-        }
+        },
+        "correct": "D",
+        "source": "Training data index 0 (GBaker/MedQA-USMLE-4-options)"
+        },
 
         # Training data index 1
-        # question = "A 3-month-old baby died suddenly at night while asleep. His mother noticed that he had died only after she awoke in the morning. No cause of death was determined based on the autopsy. Which of the following precautions could have prevented the death of the baby?"
-        # choices = {
-        # "A": "Placing the infant in a supine position on a firm mattress while sleeping",
-        # "B": "Keeping the infant covered and maintaining a high room temperature",
-        # "C": "Application of a device to maintain the sleeping position",
-        # "D": "Avoiding pacifier use during sleep"
-        # }
+        # correct: A
+        {"question": "A 3-month-old baby died suddenly at night while asleep. His mother noticed that he had died only after she awoke in the morning. No cause of death was determined based on the autopsy. Which of the following precautions could have prevented the death of the baby?",
+        "choices": {
+        "A": "Placing the infant in a supine position on a firm mattress while sleeping",
+        "B": "Keeping the infant covered and maintaining a high room temperature",
+        "C": "Application of a device to maintain the sleeping position",
+        "D": "Avoiding pacifier use during sleep"
+        },
+        "correct": "A",
+        "source": "Training data index 1 (GBaker/MedQA-USMLE-4-options)"
+        },
 
-        # Training data index 1
-        # question = "A mother brings her 3-week-old infant to the pediatrician's office because she is concerned about his feeding habits. He was born without complications and has not had any medical problems up until this time. However, for the past 4 days, he has been fussy, is regurgitating all of his feeds, and his vomit is yellow in color. On physical exam, the child's abdomen is minimally distended but no other abnormalities are appreciated. Which of the following embryologic errors could account for this presentation?"
-        # choices = {
-        # "A": "Abnormal migration of ventral pancreatic bud",
-        # "B": "Complete failure of proximal duodenum to recanalize",
-        # "C": "Abnormal hypertrophy of the pylorus",
-        # "D": "Failure of lateral body folds to move ventrally and fuse in the midline"
-        # }
+        # Training data index 2
+        # correct: A
+        {"question": "A mother brings her 3-week-old infant to the pediatrician's office because she is concerned about his feeding habits. He was born without complications and has not had any medical problems up until this time. However, for the past 4 days, he has been fussy, is regurgitating all of his feeds, and his vomit is yellow in color. On physical exam, the child's abdomen is minimally distended but no other abnormalities are appreciated. Which of the following embryologic errors could account for this presentation?",
+        "choices": {
+        "A": "Abnormal migration of ventral pancreatic bud",
+        "B": "Complete failure of proximal duodenum to recanalize",
+        "C": "Abnormal hypertrophy of the pylorus",
+        "D": "Failure of lateral body folds to move ventrally and fuse in the midline"
+        },
+        "correct": "A",
+        "source": "Training data index 2 (GBaker/MedQA-USMLE-4-options)"
+        },
 
         # Test data index 0
-        # question = "A junior orthopaedic surgery resident is completing a carpal tunnel repair with the department chairman as the attending physician. During the case, the resident inadvertently cuts a flexor tendon. The tendon is repaired without complication. The attending tells the resident that the patient will do fine, and there is no need to report this minor complication that will not harm the patient, as he does not want to make the patient worry unnecessarily. He tells the resident to leave this complication out of the operative report. Which of the following is the correct next action for the resident to take?"
-        # choices = {
-        # "A": "Disclose the error to the patient and put it in the operative report",
-        # "B": "Tell the attending that he cannot fail to disclose this mistake",
-        # "C": "Report the physician to the ethics committee",
-        # "D": "Refuse to dictate the operative report"
-        # }
+        # correct: B
+        {"question": "A junior orthopaedic surgery resident is completing a carpal tunnel repair with the department chairman as the attending physician. During the case, the resident inadvertently cuts a flexor tendon. The tendon is repaired without complication. The attending tells the resident that the patient will do fine, and there is no need to report this minor complication that will not harm the patient, as he does not want to make the patient worry unnecessarily. He tells the resident to leave this complication out of the operative report. Which of the following is the correct next action for the resident to take?",
+        "choices":{
+        "A": "Disclose the error to the patient and put it in the operative report",
+        "B": "Tell the attending that he cannot fail to disclose this mistake",
+        "C": "Report the physician to the ethics committee",
+        "D": "Refuse to dictate the operative report"
+        },
+        "correct": "B",
+        "source": "Test data index 0 (GBaker/MedQA-USMLE-4-options)"
+        },
 
-        formated_choices = format_choices(choices)
+        ]
 
-        model_prompt = prompt.format(question = question, choices = formated_choices)
+        for example in examples:
+            formated_choices = format_choices(example["choices"])
+    
+            model_prompt = prompt.format(question = example["question"], choices = formated_choices)
+    
+            # print(model_prompt)
+    
+            output_text = run_inference(model_prompt, self.model, self.tokenizer, max_new_tokens = 1024, temperature = self.temperature)
+            # output_text = output_text.split("<|assistant|>")[-1]
+            print("model output:", output_text)
+            print("\nsource: ", example["source"])
+            print("correct: ", example["correct"])
+            print("\n\n")
 
-        # print(model_prompt)
-
-        output_text = run_inference(model_prompt, self.model, self.tokenizer, max_new_tokens = 1024, temperature = self.temperature)
-        # output_text = output_text.split("<|assistant|>")[-1]
-        print(output_text)
-
-    def test_MedQA_test_data_accuracy(self):
+    def test_MedQA_test_data_accuracy(self, is_ensemble = False):
 
         MAX_TOKEN_OUTPUT = 1024
-        ENGINE_TEMPERATURE = self.temperature
         SPLIT = "test"
         DATA_RANGE = None #range(0, 100)
 
@@ -118,6 +140,29 @@ class TestPerformance():
             if match:
                 return f"({match})"
             return "[invalid]"
+
+        def run_inference_get_answer_letter(inputs, temperature):
+            with torch.no_grad():
+                outputs = self.model.generate(
+                    inputs,
+                    max_new_tokens = MAX_TOKEN_OUTPUT,
+                    do_sample=True,
+                    temperature = temperature,
+                )
+
+            # print("temperature:", temperature)
+            # print("inputs", inputs)
+            
+            text = self.tokenizer.batch_decode(outputs)[0]
+            # print("outputs text:", text)
+            text = text.split("<|assistant|>")[-1]
+            # answer = tokenizer.decode(output[0], skip_special_tokens = True)
+
+            answer = extract_answer(text).strip("()")
+            
+            print(f"answer: {answer}")
+            
+            return answer
                 
         def get_medqa_accuracy():
             # Load MedQA dataset
@@ -125,8 +170,8 @@ class TestPerformance():
             med_qa = load_dataset("GBaker/MedQA-USMLE-4-options", trust_remote_code = True)
             keys = med_qa.keys()
             # print(len(med_qa["train"]), len(med_qa["validation"]), len(med_qa["test"]))
-            
-            print ("model: ", self.model.name_or_path)
+
+            print(f"model: {self.model.name_or_path}")
             
             start_time = time.time()
             
@@ -153,29 +198,19 @@ class TestPerformance():
                 # break
                 
                 # print("correct answer: ", answer_idx)
-                
-                # inputs = tokenizer(model_prompt, return_tensors = "pt", padding=False).to(device)
-                with torch.no_grad():
-                    outputs = self.model.generate(
-                        inputs,
-                        max_new_tokens = MAX_TOKEN_OUTPUT,
-                        do_sample=True,
-                        temperature = ENGINE_TEMPERATURE,
-                        
-                        # Need these two?
-                        # attention_mask=inputs["attention_mask"],  # Use attention mask
-                        # pad_token_id=tokenizer.eos_token_id,  # Set pad token ID
-                        # use_cache = False, # For "microsoft/Phi-3-mini-4k-instruct"
-                        # do_sample = False
-                    )
-                
-                text = self.tokenizer.batch_decode(outputs)[0]
-                # print("outputs text:", text)
-                text = text.split("<|assistant|>")[-1]
-                # answer = tokenizer.decode(output[0], skip_special_tokens = True)
 
-
-                answer = extract_answer(text).strip("()")
+                if is_ensemble:
+                    answer_dict = {}
+                    for i in range(0, 5):
+                        # inputs = tokenizer(model_prompt, return_tensors = "pt", padding=False).to(device)
+                        current_answer = run_inference_get_answer_letter(inputs, temperature = 0.7)
+                        if current_answer in answer_dict:
+                            answer_dict[current_answer] += 1
+                        else:
+                            answer_dict[current_answer] = 1
+                    answer = max(answer_dict, key = answer_dict.get)
+                else:
+                    answer = run_inference_get_answer_letter(inputs, temperature = self.temperature)
                 
                 correct_answer = answer_idx
             
@@ -187,7 +222,7 @@ class TestPerformance():
                     correct_count += 1
             
                 count += 1
-                
+
                 print(f"question {count}/{len(data_list)} answer:{answer} correct_answer:{correct_answer} {is_correct}")
             
             accuracy = correct_count / count
@@ -195,7 +230,7 @@ class TestPerformance():
             
             finish_time = time.time()
             elapse_time = finish_time - start_time
-            print("elapse_time: ", elapse_time)
+            print(f"elapse_time: {elapse_time}")
 
             return accuracy
 
